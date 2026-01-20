4907f5cab4755aaa032861478f0d73af5e8d0f7b
LbuprofenL <LbuprofenL2019@outlook.com>
2026-01-20 21:47:27 +0800

    test actions

--- test/test_infer.py
+++ test/test_infer.py
@@ -82,7 +82,7 @@ def llaisys_infer(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", default="cpu", choices=["cpu", "nvidia"], type=str)
-    parser.add_argument("--model", default=None, type=str)
+    parser.add_argument("--model", default="/root/lxl/models/llaisys/DeepSeek-R1-Distill-Qwen-1.5B/", type=str)
     parser.add_argument("--prompt", default="Who are you?", type=str)
     parser.add_argument("--max_steps", default=128, type=int)
     parser.add_argument("--top_p", default=0.8, type=float)
