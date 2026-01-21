947da7dc081afb5e9291876f809b031df420a6e5
LbuprofenL <1248636101@qq.com>
2026-01-22 03:15:05 +0800

    format code

--- src/ops/embedding/cpu/embedding_cpu.hpp
+++ src/ops/embedding/cpu/embedding_cpu.hpp
@@ -4,5 +4,6 @@
 #include <cstddef>
 
 namespace llaisys::ops::cpu {
-void embedding(std::byte *out, const std::byte *index, const std::byte *weight,llaisysDataType_t type,const size_t idx_numel,const size_t dim0,const size_t dim1);
+void embedding(std::byte *out, const std::byte *index, const std::byte *weight,
+               llaisysDataType_t type, const size_t idx_numel, const size_t dim0, const size_t dim1);
 }
